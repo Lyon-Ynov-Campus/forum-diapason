58eb868770653b9148ab29e6d205e7074c583d66
Quochuy203 <quochuy.pham@ynov.com>
2026-05-05 00:26:36 +0200

    data base first step

--- models/user.go
+++ models/user.go
@@ -1,9 +1,20 @@
 package models
 
-// Compléter avec les champs nécessaires.
+import "time"
 
 type User struct {
-	ID    int    `json:"id"`
-	Email string `json:"email"`
-	Pseudo string `json:"pseudo"`
+	ID        int       `json:"id"`
+	Nom       string    `json:"nom"`
+	Pseudo    string    `json:"pseudo"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	PhotoURL  string    `json:"photo_url"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Session struct {
+	ID        string    `json:"id"`
+	UserID    int       `json:"user_id"`
+	ExpiresAt time.Time `json:"expires_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
